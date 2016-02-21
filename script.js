580d76e59efc2312827f76d2b2e76bd521ca0159
austinsprague <sprague.ak@gmail.com>
2016-02-20 21:00:31 -0800

    Added color randomizer

--- script.js
+++ script.js
@@ -1,72 +1,21 @@
 // Your JS goes here
-var redBlack = function (){
+var checkerboard = function (){
   for (var i = 0; i < 72; i++) {
+    var getHex ='#' + Math.random().toString(16).slice(2, 8);
     var square = document.createElement('div');
     square.className = 'square';
     square.style.width = '11.1%';
     square.style.float = 'left';
     square.style.paddingBottom = '11.1%';
-    square.style.backgroundColor = 'black';
+    square.style.backgroundColor = (getHex);
 
-    if ([i] % 2) {
-      square.style.backgroundColor = 'red';
-    }
+    // if ([i] % 2) {
+    //   square.style.backgroundColor = 'red';
+    // }
     document.getElementsByClassName('div');
     document.body.appendChild(square);
   }
 
 }
 
-redBlack();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
-//
-// var redBlack = function(){
-//   // var square = document.createElement('div');
-//   // square.className = 'square';
-//
-//   for (var i = 0; i < 70; i++) {
-//     if (i % 2) {
-//     square.style.color="red"
-//   }else{
-//     square.style.color="red"
-//   }
-//     document.getElementsByClassName('square');
-//     div.appendChild('div')[i];
-//   }
-//
-// }
-//
-// redBlack();
+checkerboard();
