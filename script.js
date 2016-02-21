a4c83ce2420fd55045bc39f859fa5e783952957a
austinsprague <sprague.ak@gmail.com>
2016-02-20 20:35:04 -0800

    Modified JS to checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,72 @@
 // Your JS goes here
+var redBlack = function (){
+  for (var i = 0; i < 72; i++) {
+    var square = document.createElement('div');
+    square.className = 'square';
+    square.style.width = '11.1%';
+    square.style.float = 'left';
+    square.style.paddingBottom = '11.1%';
+    square.style.backgroundColor = 'black';
+
+    if ([i] % 2) {
+      square.style.backgroundColor = 'red';
+    }
+    document.getElementsByClassName('div');
+    document.body.appendChild(square);
+  }
+
+}
+
+redBlack();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//
+// var redBlack = function(){
+//   // var square = document.createElement('div');
+//   // square.className = 'square';
+//
+//   for (var i = 0; i < 70; i++) {
+//     if (i % 2) {
+//     square.style.color="red"
+//   }else{
+//     square.style.color="red"
+//   }
+//     document.getElementsByClassName('square');
+//     div.appendChild('div')[i];
+//   }
+//
+// }
+//
+// redBlack();
