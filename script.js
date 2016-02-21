71a684419b6399f4cc2824cc52f231f0930b1490
austinsprague <sprague.ak@gmail.com>
2016-02-20 21:04:28 -0800

    Updated syntax

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-// Your JS goes here
 var checkerboard = function (){
   for (var i = 0; i < 72; i++) {
     var getHex ='#' + Math.random().toString(16).slice(2, 8);
     var square = document.createElement('div');
+
     square.className = 'square';
     square.style.width = '11.1%';
     square.style.float = 'left';
     square.style.paddingBottom = '11.1%';
     square.style.backgroundColor = (getHex);
 
-    // if ([i] % 2) {
-    //   square.style.backgroundColor = 'red';
-    // }
     document.getElementsByClassName('div');
     document.body.appendChild(square);
   }
