b26117a29d0c61261975fa13e4b922308e70fd79
austinsprague <sprague.ak@gmail.com>
2016-02-24 11:51:22 -0800

    Update syntax with lint

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
-// Your JS goes here
-var redBlack = function (){
+(function() {
+  'use strict';
   for (var i = 0; i < 72; i++) {
     var square = document.createElement('div');
+
     square.className = 'square';
     square.style.width = '11.1%';
     square.style.float = 'left';
     square.style.paddingBottom = '11.1%';
     square.style.backgroundColor = 'black';
 
-    if ([i] % 2) {
+    if (i % 2) {
       square.style.backgroundColor = 'red';
     }
     document.getElementsByClassName('div');
     document.body.appendChild(square);
   }
-
-}
-
-redBlack();
+})();
